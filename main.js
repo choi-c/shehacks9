e0e12963408160d2ceb4d91414305e76d7fe6cdc
choi-c <carmenchoi333@gmail.com>
2025-01-11 17:00:58 -0500

    style and index changes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Testing function
+/*
 function myFunction(){
     document.getElementById("p").innerHTML = "hihihihihi";
 }
 let x = document.createElement("text");
 x.setAttribute("type","text");
+*/
+console.log("hello world");
